954dc09d0758d695a8ff07f058fe65024bf43735
nimishaCodes <62233955+nimishaCodes@users.noreply.github.com>
2021-09-09 17:19:02 +0800

    comment

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+//comments 123
 namespace MyMicroservice
 {
     public class Program
