74d0bfcbd1b1503ca65297943500a422f63f25ab
nimishaCodes <lnimishakavi@gmail.com>
2021-09-09 17:48:18 +0800

    added comment again

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 //comments 123 45
+//this is fixing ui stuff
 namespace MyMicroservice
 {
     public class Program
